1271f5a468e3d70e83bef316736c3b921b20a7da
fazemodz <hike820@gmail.com>
2021-06-22 13:05:20 +0100

    added auto links

--- frontend/src/modules/APIcalls/YoutubeAPI/getVideoDetails.js
+++ frontend/src/modules/APIcalls/YoutubeAPI/getVideoDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Linkify from 'react-linkify'
 //Z3RA7bi5FUM  https://youtu.be/Z3RA7bi5FUM https://youtube.com/Z3RA7bi5FUM 
 export default class getVidGetVideoDetailseoDetails extends Component {
     state = {
@@ -8,12 +9,12 @@ export default class getVidGetVideoDetailseoDetails extends Component {
         SnippetArray: [],
         thumbnailsArray: [],
         ChannelDataArray: [],
-        ChannelSnippetArray:[],
-        ChannelThumbnailArray:[],
-        ChannelStatisticsArray:[]
+        ChannelSnippetArray: [],
+        ChannelThumbnailArray: [],
+        ChannelStatisticsArray: []
     }
     GetAPIData(NewYoutubeURL) {
-        
+
         axios.get(`http://localhost:5000/api/v1/youtubeapi/getvideostats/${NewYoutubeURL}`)
             //axios.get('http://localhost:5000/')
             .then((response) => {
@@ -28,33 +29,33 @@ export default class getVidGetVideoDetailseoDetails extends Component {
                 console.log(error)
             })
     }
-    GetChannelData(){
+    GetChannelData() {
         let ChannelID = this.state.SnippetArray.channelId;
         axios.get(`http://localhost:5000/api/v1/youtubeapi/getchannelstats/${ChannelID}`)
-        .then((response)=>{
-            this.setState({ChannelDataArray: response.data.items[0]});
-            this.setState({ChannelSnippetArray: response.data.items[0].snippet})
-            this.setState({ChannelThumbnailArray: response.data.items[0].snippet.thumbnails.medium})
-            this.setState({ChannelStatisticsArray: response.data.items[0].statistics})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+            .then((response) => {
+                this.setState({ ChannelDataArray: response.data.items[0] });
+                this.setState({ ChannelSnippetArray: response.data.items[0].snippet })
+                this.setState({ ChannelThumbnailArray: response.data.items[0].snippet.thumbnails.medium })
+                this.setState({ ChannelStatisticsArray: response.data.items[0].statistics })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
-    YoutubrURLCheck(){
-        
-        if(this.state.YoutubeURL.includes("https://www.youtube.com/watch?v=")){
+    YoutubrURLCheck() {
+
+        if (this.state.YoutubeURL.includes("https://www.youtube.com/watch?v=")) {
             let NewYoutubeURL = this.state.YoutubeURL.slice(32);
             this.setState({
                 YoutubeURL: '',
             })
             this.GetAPIData(NewYoutubeURL)
             this.RenderData()
-        }else;
-        if(this.state.YoutubeURL.includes("https://youtu.be/")){
+        } else;
+        if (this.state.YoutubeURL.includes("https://youtu.be/")) {
             let NewYoutubeURL2 = this.state.YoutubeURL.slice(17)
-           
-        }else;
+
+        } else;
     }
     ChangeHandler = e => {
         this.setState({
@@ -64,27 +65,31 @@ export default class getVidGetVideoDetailseoDetails extends Component {
     onSubmit = e => {
         e.preventDefault();
         this.YoutubrURLCheck();
-        
+
     }
     RenderData() {
-        
+
         return (
             <div>
-                <pre>
-                    <p>{this.state.SnippetArray.title}</p>
-                    <p>{this.state.SnippetArray.description}</p>
-                    <img src={this.state.thumbnailsArray.url}/>
-                </pre>
-                <pre>
-                    <p>{this.state.ChannelSnippetArray.title}</p>
-                    <p>{this.state.ChannelSnippetArray.description}</p>
-                    <p>{this.state.ChannelSnippetArray.customUrl}</p>
-                    <p>{this.state.ChannelSnippetArray.publishedAt}</p>
-                    <img src={this.state.ChannelThumbnailArray.url}/>
-                    <p>Total View Count: {this.state.ChannelStatisticsArray.viewCount}</p>
-                    <p>Total Subs: {this.state.ChannelStatisticsArray.subscriberCount}</p>
-                    <p>Total video Count: {this.state.ChannelStatisticsArray.videoCount}</p>
-                </pre>
+                <Linkify properties={{target: '_blank',}}>
+                    <pre>
+                        <p>{this.state.SnippetArray.title}</p>
+                        <p>{this.state.SnippetArray.description}</p>
+                        <img src={this.state.thumbnailsArray.url} />
+                    </pre>
+                    </Linkify>
+                    <Linkify properties={{target: '_blank',}}>
+                    <pre>
+                        <p>{this.state.ChannelSnippetArray.title}</p>
+                        <p>{this.state.ChannelSnippetArray.description}</p>
+                        <p>{this.state.ChannelSnippetArray.customUrl}</p>
+                        <p>{this.state.ChannelSnippetArray.publishedAt}</p>
+                        <img src={this.state.ChannelThumbnailArray.url} />
+                        <p>Total View Count: {this.state.ChannelStatisticsArray.viewCount}</p>
+                        <p>Total Subs: {this.state.ChannelStatisticsArray.subscriberCount}</p>
+                        <p>Total video Count: {this.state.ChannelStatisticsArray.videoCount}</p>
+                    </pre>
+                </Linkify>
             </div>
         )
     }
@@ -98,10 +103,10 @@ export default class getVidGetVideoDetailseoDetails extends Component {
                         value={this.state.YoutubeURL}
                         onChange={e => this.ChangeHandler(e)}
                     />
-                    <br /> 
+                    <br />
                     <button onClick={e => this.onSubmit(e)}>Submit</button>
                 </form>
-            {this.RenderData()}
+                {this.RenderData()}
             </div>
         )
     }
